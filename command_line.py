7f222a9e7d4558688b95fdd8ee5afd4222124ca2
TNTNINJA2 <108622789+TNTNINJA2@users.noreply.github.com>
2024-11-08 17:53:35 -0800

    Completed Tasks 1-3

--- command_line.py
+++ command_line.py
@@ -1,1 +1,15 @@
 
+import sys
+
+
+if __name__ == "__main__":
+    print(sys.argv)
+
+'''
+Testing
+Script Parameters:
+These are the arguments.
+
+Console Output:
+['C:\\Users\\yosem\\Documents\\GitHub\\CP CS 101\\Week 7\\lab7-tylerpike0\\command_line.py', 'These', 'are', 'the', 'arguments.']
+'''
--- convert_tests.py
+++ convert_tests.py
@@ -1,1 +1,15 @@
+import unittest
+from convert import str_to_float
+class ConvertTests(unittest.TestCase):
 
+    def test_str_to_float(self):
+        input = "-125.2"
+        expected = -125.2
+        actual = str_to_float(input)
+        self.assertEqual(expected,actual)
+
+    def test_str_to_float(self):
+        input = ".-125.2"
+        expected = None
+        actual = str_to_float(input)
+        self.assertEqual(expected,actual)
